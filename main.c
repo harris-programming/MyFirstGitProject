4667753a274e90ef37120bbabf59c2affea50c4d
Harris <hlsazali19@gmail.com>
2026-09-23 11:50:36 +0800

    Add Developer B message

--- main.c
+++ main.c
@@ -3,5 +3,6 @@ int main() {
     printf("Hello Git!\n");
     printf("This is my practice branch!\n");
     printf("This is a test feature!\n");
+    printf("Hello from Developer B!\n");
     return 0;
 }
